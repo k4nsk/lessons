62f8480dcaf93edbb95c1c0df7117821a013740c
k4nsk <e.kichinskayaa@gmail.com>
2023-02-17 16:37:27 +0300

    Adventure Game

---
+++ Game/src/Room.java
@@ -0,0 +1,8 @@
+public class Room {
+    public String name;
+    public String description;
+
+    public void printItems() {
+        System.out.println("В этой комнате нет доступных предметов");
+    }
+}
